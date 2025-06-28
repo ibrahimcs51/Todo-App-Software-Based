c11f6b21774794502e23c9e38686b49c6decb786
ibrahimcs51 <engribrahimkhanokz@gmail.com>
2025-06-28 07:12:51 +0500

    Update axios instance base URL to a fixed endpoint

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://todo-app-backend-inky.vercel.app/api' , 
   headers: {
     'Content-Type': 'application/json',
   },
